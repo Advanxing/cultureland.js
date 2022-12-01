5d4ed3797821caecd15bb1595469c086baa87ca4
DollarNoob <35988515+DollarNoob@users.noreply.github.com>
2022-12-02 00:17:13 +0900

    Update for cultureland.js

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const cultureland = require("./cultureland");
 (async function() {
     const client = new cultureland();
     await client.login("keepLoginInfo").then(console.log).catch(console.error);
-    await client.charge("0000-0000-0000-000000", false).then(console.log).catch(console.error);
+    await client.charge(["0000", "0000", "0000", "000000"], false).then(console.log).catch(console.error);
 })();
