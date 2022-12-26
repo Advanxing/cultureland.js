9357eca19e6461ed7dedc3d142e73a941841fa67
DollarNoob <35988515+DollarNoob@users.noreply.github.com>
2022-12-27 02:23:48 +0900

    Remove unused CycleTLS options

--- cultureland.js
+++ cultureland.js
@@ -75,7 +75,6 @@ class cultureland {
                 "content-type": "application/x-www-form-urlencoded",
                 cookie: this.cookies.join("; ")
             },
-            method: "POST",
             body: requestBody,
             ja3: "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513-21,29-23-24,0",
             userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
