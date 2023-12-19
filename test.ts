6e00d224c80acf5e56b773d87458ee8a415210ff
LunarLight <80996357+mzggr0914@users.noreply.github.com>
2023-12-19 23:46:17 +0900

    잘못 만든거 삭제

--- test.ts
+++
@@ -1,5 +0,0 @@
-import clipboardy from 'clipboardy';
-import cheerio from 'cheerio';
-const $ = cheerio.load(clipboardy.readSync());
-const cashChargeAmtValue = $('#cashChargeAmt').val()
-console.log(cashChargeAmtValue)
