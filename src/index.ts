b38c95c0981ede896492e2f400787ab762743a13
Not-swasd <swasd@swasd.net>
2024-07-14 18:02:08 +0900

    Update export for types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./Cultureland.js";
 export * from "./CulturelandError.js";
 export * as TransKey from "./mTranskey/index.js";
 export * from "./Pin.js";
-export * from "./types.js";
+export * as Types from "./types.js";
 
