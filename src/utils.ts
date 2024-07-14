f4620f72566c478e6fee51f008d19ddd72f6c719
Not-swasd <swasd@swasd.net>
2024-07-14 11:21:54 +0900

    add randomString function

---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function randomString(length: number) {
+    return new Array(length).fill("").map(() => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".charAt(Math.random() * 62)).join("");
+}
