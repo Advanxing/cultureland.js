1b6f0abd342d3ac5ab579cfaa07c49d93cd04ccc
jshp3795 <dollar3795@hotmail.com>
2024-08-09 04:58:00 +0900

    CulturelandError.toString()

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export * from "./Cultureland.js";
+export { default } from "./Cultureland.js";
 export * from "./CulturelandError.js";
 export * as TransKey from "./mTranskey/index.js";
 export * from "./Pin.js";
 export * as Types from "./types.js";
-
